59c2c2ac0b41b067ee7250e53cd9bca3f1fe4863
Alphaeus Ng <78588510+AlphaeusNg@users.noreply.github.com>
2026-09-04 23:42:14 +0800

    fix(ui): load app via joined parts (MCP size workaround)

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-@workspace/CardFitSG/js/app.js
+(function () {
+  "use strict";
+  var texts = [];
+  for (var i = 0; i < 3; i++) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "js/app.part" + i + ".txt", false);
+    xhr.send(null);
+    if (xhr.status !== 200 && xhr.status !== 0) {
+      throw new Error("CardFitSG failed to load js/app.part" + i + ".txt (HTTP " + xhr.status + ")");
+    }
+    texts.push(xhr.responseText);
+  }
+  (0, eval)(texts.join(""));
+})();
